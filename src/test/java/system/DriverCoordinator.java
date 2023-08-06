52918bab3ffc51c68c69946e46e3bc743311c749
Marek Dziekan <sheriour@gmail.com>
2023-08-06 23:35:44 +0100

    Trying with headed, and docker

--- src/test/java/system/DriverCoordinator.java
+++ src/test/java/system/DriverCoordinator.java
@@ -4,8 +4,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 public class DriverCoordinator
@@ -16,15 +19,21 @@ public class DriverCoordinator
         return webDriver.get() != null;
     }
 
+    public static String remote_url_chrome = "http://selenium:4444/wd/hub";
+
     public static WebDriver getWebDriver(){
         WebDriver driver = webDriver.get();
         if (driver == null) {
-
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
             options.addArguments("--headless");
-            driver = new ChromeDriver(options);
+            //driver = new ChromeDriver(options);
+            try {
+                driver = new RemoteWebDriver(new URL(remote_url_chrome), options);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
             webDriver.set(driver);
             driver.manage().window().maximize();
         }
