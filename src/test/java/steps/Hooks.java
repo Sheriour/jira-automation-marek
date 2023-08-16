8e367481a17b857f84c23e500a59c4acdd263daf
Marek Dziekan <sheriour@gmail.com>
2023-08-16 22:55:01 +0100

    Added screenshot support in reports

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -1,6 +1,8 @@
 package steps;
 
 import io.cucumber.java.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import system.DriverCoordinator;
 import system.EntityManager;
 import system.PageRepository;
@@ -13,9 +15,16 @@ public class Hooks
     }
 
     @After
-    public void teardown(){
-        if (DriverCoordinator.hasDriver())
+    public void teardown(Scenario scenario){
+        if (DriverCoordinator.hasDriver()) {
+            if (scenario.isFailed()) {
+                // Take a screenshot...
+                final byte[] screenshot = ((TakesScreenshot)DriverCoordinator.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+                // embed it in the report.
+                scenario.attach(screenshot, "image/png", "screenshot");
+            }
             DriverCoordinator.quitWebDriver();
+        }
         PageRepository.deleteAllPages();
     }
 }
